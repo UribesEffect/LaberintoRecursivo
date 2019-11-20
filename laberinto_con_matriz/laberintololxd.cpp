c911720bfa5a9e984a1e4e2f3e3d5834301be388
UribesEffect <38766156+UribesEffect@users.noreply.github.com>
2019-11-20 15:29:22 -0500

    Update laberintololxd.cpp
    este ya es el archivo completamente hecho y comentado, toca pasarlo a  cpp y main de ser necesario hpp.

--- laberinto_con_matriz/laberintololxd.cpp
+++ laberinto_con_matriz/laberintololxd.cpp
@@ -1,194 +1,229 @@
-#include <stdio.h>
+#include <fstream>
+#include <iostream>
+using namespace std;
 
-#include <stdlib.h>
+int matriz[100][100]; // Matriz que guarda los datos del archivo. El maximo del laberinto son 100 filas por 100 columnas.
 
-#include <ctype.h>
+int filas,columnas; // filas y columnas del laberinto
 
+int filapersona,columnapersona; // coordenadas del jugador
 
-
-int m[100][100]; // Matriz que guarda los datos del archivo. El maximo del laberinto son 100 filas por 100 columnas.
-
-                 //Se puede modificar
-
-
-
-int f,c; // filas y columnas del laberinto
-
-int fp,cp; // coordenadas del jugador
-
-int fs,cs; // coordenadas de la salida o meta
+int filasalida,columnasalida; // coordenadas de la salida o meta
 
 const int ARRIBA=0, DERECHA=1, IZQUIERDA=2, ABAJO=3;
 
 int direccion=ARRIBA; // El jugador inicia viendo hacia arriba
 
-int arriba, abajo, izquierda, derecha; // Aqui guardamos la informacion de la matriz de las
-
-                                             // cuatro direcciones junto al jugador
-
-
+int arriba, abajo, izquierda, derecha; // Con estos atributos guardamos la informacion de la matriz para girar a dicho lado
+                                       // Es decir las direcciones que tiene la persona
 
 void leerArchivo() {
     int maxCol;
-    FILE *archivo;
+    ifstream archivo;
     char caracter;
-    int codigo;
-    bool flag=true; // Sirve para que aumente el numero de columnas solo hasta que encuentre le primer salto de linea
+    int caracterAscii;
+    bool boolcolumnas=true; // Sirve para que aumente el numero de columnas solo hasta que encuentre le primer salto de linea
 
                // suponiendo que el archivo tiene todas las lineas de igual numero de caracteres
 
 
+        string a = "laberinto.txt"; //guardamos el nombre del laberinto para imprimirlo al final de que si lo ejecuto
+        archivo.open(a); // abrimos el archivo que tiene el laberinto que queremos resolver
 
-        archivo=fopen("laberinto2.txt","rt"); // abrimos el archivo "laberinto.txt" en modo lectura de texto("rt"
-
-        if (archivo==NULL) { // En caso de que el archivo no exista o que este mal el nombre
-           printf("Error en lectura de archivo\n");
-           system("pause");
-           exit (EXIT_FAILURE); // Terminacion del programa por falla
+        if (!archivo.is_open()) { // Si el archivo no existe o esta mal el nombre se ejecuta
+           cout <<"Error en lectura de archivo\n";
+           exit(true); // se acaba el programa por error de no encontrar el archivo
         }
 
-        while(!feof(archivo)) { // Este ciclo sirve para dimensionar nuestra matriz
-                caracter=fgetc(archivo);
-                codigo = caracter; // obtenemos el codigo Ascii de el caracter
+        while(!archivo.eof()) { // sirve para llegar al final de la lectura del archivo
+                caracter = archivo.get();//asignamos a caracter cada uno de los chars del txt
+                caracterAscii = caracter; // obtenemos el codigo Ascii de el caracter
 
-                if (codigo==10) { // codigo del salto de linea
-                   f++; // Si encontramos un enter añadimos una fila mas a nuestra matriz
-                   if (flag) {maxCol=c; flag=false;} // solo lo hacemos una vez
-                   c=0;
+                if (caracterAscii == 10) { // 10 se refiere al codigo Ascii del salto de lineas
+
+
+                     filas++; //Añadimos una fila a nuestra matriz
+
+                     if (boolcolumnas){// solo lo hacemos para asignar el maximo de columnas
+                          maxCol = columnas;
+                          boolcolumnas = false;
+                     }
+
+                     columnas = 0;
+                     cout << '\t';
+
                 } else {
-                       m[f][c]=codigo-48;
-                       if (m[f][c]==2){fp=f;cp=c;}
-                       if (m[f][c]==3){fs=f;cs=c;}
-                       c++ ;// añadimos una columna mas a nuestra matriz
+                       matriz[filas][columnas] = caracterAscii - 48;
+                       if ( matriz[filas][columnas] == 2){
+                         filapersona = filas; columnapersona = columnas;
+                       }
+                       if ( matriz[filas][columnas] == 3){
+                         filasalida = filas;columnasalida = columnas;
+                       }
+                       columnas++ ;// añadimos una columna mas a nuestra matriz
                 }
         }
 
-        c=maxCol; // si no ponemos esto la matriz queda de una columna
-        fclose(archivo);
-        //printf ("%d filas, %d columnas",f,maxCol); // para debug
+        columnas = maxCol; // aqui ya se asigna la cantidad de columnas de la matriz
+        cout << "########################################################" << '\n';
+        cout << "# Archivo leido exitosamente y se llama: " << a <<" #\n";
+        cout << "###      A continuacion el inicio del laberinto" << "      ###\n";
+        cout << "########################################################" << '\n';
+        archivo.close(); // cerramos el archivo
+
 
 }
 
 
 
 void MuestraLaberinto() {
-     // En esta funcion las paredes se muestran como asteriscos "*", el jugador como "J" y la meta como "M"
-    system("cls");
-    for (int i=0;i<f;i++) {
-        for (int ii=0;ii<c;ii++) {
-           if (m[i][ii]==1) printf ("|");
-           else if (i==fs && ii==cs) printf ("M");
-           else if (i==fp && ii==cp) printf ("J");
-           else printf (" ");
+     // En esta funcion cambiamos el txt que tomamos en leer archivo  y lo convertimos en:
+     // los 0 ahora son ' '
+     // los 1 ahora son '|'
+     // el 2 ahora es 'P' que se refiere a la persona
+     // el 3 ahora es 'S' que se refiere a la salida
+
+    for (int i = 0;i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+           if (matriz[i][j] == 1) cout << "|";
+           else if (i == filapersona && j == columnapersona) cout << "P";
+           else if (i == filasalida && j == columnasalida) cout << "S";
+           else cout << " ";
         }
-    printf ("\n");
+    cout << "\n";
     }
-    //printf ("Direccion %d, arriba %d, abajo %d, derecha %d, izquierda %d\n",direccion,arriba,abajo,derecha,izquierda); // debug
-    system("pause"); // esperamos que el usuario presione una tecla
+    cout << "###################################################" << '\n';//para separar un laberinto de otro
 
 }
 
 void RecorrerLaberinto() {
-    int old_fp,old_cp; // Las usaremos para poner un espacio en donde estaba el jugador
+    int fila_antigua_persona,columna_antigua_persona; // con estos enteros guardaremos la pasada posicion de la persona para poner un espacio
 
-      old_fp=fp; old_cp=cp; // Guardamos las coordenadas del jugador, para colocar un espacio cuando se mueva
+      fila_antigua_persona = filapersona; //asignamos la fila de la persona antes de moverse
+      columna_antigua_persona = columnapersona; // asignamos la columna de la persona antes de moverse
 
-      arriba=m[fp-1][cp];
+      arriba = matriz[filapersona-1][columnapersona];// asignamos lo que significa en la matriz moverse para arriba
 
-      abajo=m[fp+1][cp];
+      abajo = matriz[filapersona+1][columnapersona];// asignamos lo que significa en la matriz moverse para abajo
 
-      derecha=m[fp][cp+1];
+      derecha = matriz[filapersona][columnapersona+1];// asignamos lo que significa en la matriz moverse para la derecha
 
-      izquierda=m[fp][cp-1];
+      izquierda = matriz[filapersona][columnapersona-1];// asignamos lo que significa en la matriz moverse para la izquierda
 
-      // checamos si la meta esta junto al jugador
-      if (derecha==3) direccion=DERECHA;
-      else if (izquierda==3) direccion=IZQUIERDA;
-      else if (arriba==3) direccion=ARRIBA;
-      else if (abajo==3) direccion=ABAJO;
+      //############################################
+      //### si la meta esta al lado de la persona###
+      //############################################
+      if (derecha == 3)
+          direccion = DERECHA;
+      else if (izquierda == 3)
+          direccion = IZQUIERDA;
+      else if (arriba == 3)
+          direccion = ARRIBA;
+      else if (abajo == 3)
+          direccion = ABAJO;
+      //###############################################
 
 
-
-      // Si no estamos junto a la meta, avanzamos
-      else if (direccion==ARRIBA){ // volteando al norte
-         if (derecha==1 && arriba==0) {}// Antes de avanzar checamos si hay pared a la derecha
-         else if (derecha==0) direccion=DERECHA;// si no hay pared a la derecha giramos viendo a la derecha
-         else if (izquierda==0) direccion=IZQUIERDA;// si no, si no hay pared en la izq. giramos viendo hacia alla
-         else direccion=ABAJO;// ultima opcion, abajo giramos viendo hacia abajo
-
-      }
-      else if (direccion==1){ // volteando al este/derecha
-         if (abajo==1 && derecha==0) {}// Antes de avanzar hacia la derecha checamos si hay pared abajo
-         else if (abajo==0) direccion=ABAJO;// si no hay pared abajo giramos a la derecha, mirando abajo
-         else if (arriba==0) direccion=ARRIBA;// si no hay pared arriba giramos
-         else direccion=IZQUIERDA;// ultima opcion la izquierda
-
-      }
-      else if (direccion==3){ // volteando al sur
-
-         if (abajo==0 && izquierda==1) {}// si no hay pared abajo y a la izq. si, avanzamos
-         else if (izquierda==0) direccion=IZQUIERDA;// si no hay pared a la izquierda giramos
-         else if (derecha==0) direccion=DERECHA;// si no hay pared a la derecha giramos
-         else direccion=ARRIBA; // ultima opcion arriba
+      //#########################################
+      //### Si la meta no esta al lado nuestro###
+      //#########################################
+      else if (direccion == ARRIBA){ // miramos hacia arriba
+         if (derecha == 1 && arriba == 0) {}// miramos si tenemos pared a la derecha
+         else if (derecha == 0) // si no tenemos pared a la derecha miramos hacia alla
+            direccion = DERECHA;
+         else if (izquierda == 0) // si hay pared a la derecha miramos para la izquierda y so no hay pared miramos para alla
+            direccion = IZQUIERDA;
+         else
+            direccion = ABAJO;// la ultima opcion seria devolvernos es decir mirar hacia abajo
 
       }
+      else if (direccion == 1){ // si direccion es igual a 1 quiere decir que volteamos a la derecha
+         if (abajo == 1 && derecha == 0) {}// Antes de avanzar hacia la derecha checamos si hay pared abajo
 
-      else if (direccion==2){ // volteando a la izquierda
+         else if (abajo == 0)// si no hay pared abajo nos giramos a la derecha (volteamos sabiendo que estamos mirando para abajo)
+            direccion = ABAJO;
+         else if (arriba == 0)// si no hay pared arriba giramos a la izquierda (volteamos sabiendo que estamos mirando para abajo)
+            direccion = ARRIBA;
+         else
+            direccion = IZQUIERDA;// por ultimo seria la izquierda
 
-         if (izquierda==0 && arriba==1) {}// si hay pared arriba y a la izq. no, avanzamos
-         else if (arriba==0) direccion=ARRIBA;// si no hay pared arriba giramos a la derecha,
-         else if (abajo==0) direccion=ABAJO;// si no hay pared abajo giramos
-         else direccion=DERECHA;// ultima opcion volver atras, giramos
+      }
+      else if (direccion == 3){ // si direccion es igual a 3 quiere decir que volteamos para abajo
+
+         if (abajo == 0 && izquierda == 1) {}// si no tenemos paredes a los lados avanzamos
+         else if (izquierda == 0)
+            direccion = IZQUIERDA;// si no tenemos pared a la izquierda nos giramos a esta direccion
+         else if (derecha == 0)
+            direccion = DERECHA;// si no tenemos pared a la derecha nos giramos a esta direccion
+         else
+            direccion = ARRIBA; // por ultimo nos queda para arriba
 
       }
 
-      m[old_fp][old_cp]=0; // ponemos un espacio donde estaba el jugador
+      else if (direccion==2){ // si direccion es igual a 2 quiere decir que volteamos para la izquierda
 
-      switch(direccion){ // Movemos al jugador
+         if (izquierda == 0 && arriba == 1) {}// si tenemos paredes arriba y a la izquierda no, avanzamos
+
+         else if (arriba == 0)// si no hay pared arriba giramos a la derecha
+            direccion = ARRIBA;
+         else if (abajo == 0)// si no hay pared abajo nos direcionamos a dicha posicion
+            direccion = ABAJO;
+         else
+            direccion = DERECHA;// por ultimo nos devolvemos
+
+      }
+
+      matriz[fila_antigua_persona][columna_antigua_persona] = 0; // ponemos un espacio donde estaba el jugador
+
+      switch(direccion){ // con este switch movemos al jugador segun los casos(paredes) que tengamos
         case 0:
 
-             m[fp-1][cp]=2; // actualizamos matriz
-            fp--; // Actualizamos coordenadas del jugador
-
-        break;
+             matriz[filapersona-1][columnapersona]=2; // actualizamos matriz
+             filapersona--; // Actualizamos coordenadas del jugador
+             break;
 
         case 1:
 
-             m[fp][cp+1]=2; // actualizamos matriz
-             cp++; // Actualizamos coordenadas del jugador
-
-        break;
+             matriz[filapersona][columnapersona+1]=2; // actualizamos matriz
+             columnapersona++; // Actualizamos coordenadas del jugador
+             break;
 
         case 2:
 
-             m[fp][cp-1]=2; // actualizamos matriz
-            cp--; // Actualizamos coordenadas del jugador
-
-        break;
+             matriz[filapersona][columnapersona-1]=2; // actualizamos matriz
+             columnapersona--; // Actualizamos coordenadas del jugador
+             break;
 
         case 3:
 
-             m[fp+1][cp]=2; // actualizamos matriz
-            fp++; // Actualizamos coordenadas del jugador
-
-        break;
+             matriz[filapersona+1][columnapersona]=2; // actualizamos matriz
+             filapersona++; // Actualizamos coordenadas del jugador
+             break;
       }
 }
 
+void recorrido_total(){
+  int pasos = 0; //entero que guarda los movimientos de la persona
+  while (filapersona != filasalida || columnapersona != columnasalida) {// Cuando encontramos la meta, salimos del ciclo
+
+    getchar();//con esta funcion al undir una tecla del teclado continuamos con el while
+    RecorrerLaberinto();
+    pasos++;
+    MuestraLaberinto();
+
+  }
+  cout << "Cantidad de pasos dados: " << pasos <<'\n';
+}
 
 
 int main() {
 
+
     leerArchivo();
     MuestraLaberinto();
-    while (fp!=fs || cp!=cs) {// Cuando encontramos la meta, salimos del ciclo
-      RecorrerLaberinto();
-      MuestraLaberinto();
-    }
+    recorrido_total();
+    cout << "Has llegado a la meta\n";
 
-    system("cls");
-    printf ("El jugador llego a la meta\n");
-    system("pause");
     return 0;
 
 }
